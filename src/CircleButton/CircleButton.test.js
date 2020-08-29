cd48d91d6a726fbadacb8589f85ee86c64be4a8a
Tharwin <tharwin.carr08@gmail.com>
2020-08-28 20:23:10 -0700

    added test files to app

--- src/CircleButton/CircleButton.test.js
+++ src/CircleButton/CircleButton.test.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
-import NotefulForm from './NotefulForm'
+import CircleButton from './CircleButton'
 
-describe(`NotefulForm component`, () => {
+describe(`CircleButton component`, () => {
   const props = {
+    tag: 'a',
     className: 'test-class-name',
     children: <p>test children</p>,
     'data-other': 'test-other-prop'
   }
 
-  it('renders a form.NotefulForm by default', () => {
-    const wrapper = shallow(<NotefulForm />)
+  it('renders a button.CircleButton by default', () => {
+    const wrapper = shallow(<CircleButton />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('renders the NotefulForm given props', () => {
-    const wrapper = shallow(<NotefulForm {...props} />)
+  it('renders the circle button from props', () => {
+    const wrapper = shallow(<CircleButton {...props} />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
