8fac2e0374d811e408d817762b91e424ad5625ea
Tharwin <tharwin.carr08@gmail.com>
2020-09-01 01:18:58 -0700

    updated context and remove button works

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-    faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
 } from '@fortawesome/free-solid-svg-icons'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App/App';
+import App from './App/App'
 
 library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 )
