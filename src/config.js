becc9ea896b9d6dad62a782378edead019bdc441
Tharwin <tharwin.carr08@gmail.com>
2020-10-28 22:04:15 -0700

    fixed config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export default {
-    API_ENDPOINT: `http://localhost:8000/api`
-  }
-
-  //export default {
-    //API_ENDPOINT: `https://mighty-refuge-27954.herokuapp.com/api`
+//export default {
+    //API_ENDPOINT: `http://localhost:8000/api`
   //}
+
+export default {
+  API_ENDPOINT: `https://mighty-refuge-27954.herokuapp.com/api`
+}
